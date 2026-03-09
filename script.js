b029c0c2ddbbee48eec45443ab40598899d37ae8
EddyBeneteau <94656496+EddyBeneteau@users.noreply.github.com>
2026-03-09 13:41:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-function openDemo(){
-document.getElementById("demoModal").style.display="flex";
-}
-
-function closeDemo(){
-document.getElementById("demoModal").style.display="none";
-}
+console.log('DanceEdit');
